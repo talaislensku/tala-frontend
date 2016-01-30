1c75730e4d0bf39684851dbc527ad181bc30aedd
David Blurton <david.blurton@gmail.com>
2016-01-30 19:44:50 +0000

    Make links work for tags with more than 2 variants (eg gender)

--- app/links.js
+++ app/links.js
@@ -10,24 +10,28 @@ const Links = ({result, current, setCurrent}) => {
   const tags = Object.keys(current.tags)
   const linkTags = tags.filter(x => x !== 'grammarCase')
 
-  const links = linkTags.map(tag => result.forms.filter(form => (
-    form.tags[tag] !== current.tags[tag] &&
-    tags.filter(t => t !== tag).every(otherTag => form.tags[otherTag] === current.tags[otherTag])
-  ))[0]).filter(x => x)
+  const linkGroups = linkTags.map(tag => result.forms.filter(form => {
+    const otherTags = tags.filter(t => t !== tag)
+    return form.tags[tag] !== current.tags[tag] &&
+      otherTags.every(otherTag => form.tags[otherTag] === current.tags[otherTag])
+  })).filter(x => x)
 
   return (
     <div className={styles.linksSection}>
-      { links && links.map((link, index) => (
-        <div className={styles.links} key={index}>
-          <div className={styles.column}>
-            <div className={styles.linkCurrent}>{current.tags[linkTags[index]]}</div>
-            <div className={styles.linkOther}>{link.tags[linkTags[index]]}</div>
-          </div>
-          <div className={styles.columnEnd}>
-            <div className={styles.link} onClick={setCurrent.bind(null, link)}>{link.form}</div>
-          </div>
-        </div>
-      )) }
+      { linkGroups && linkGroups.map((linkGroup, index) => (
+          linkGroup.map((link) => (
+            <div className={styles.links}>
+              <div className={styles.column}>
+                <div className={styles.linkCurrent}>{current.tags[linkTags[index]]}</div>
+                <div className={styles.linkOther}>{link.tags[linkTags[index]]}</div>
+              </div>
+              <div className={styles.columnEnd}>
+                <div className={styles.link} onClick={setCurrent.bind(null, link)}>{link.form}</div>
+              </div>
+            </div>
+          ))
+        ))
+      }
     </div>
   )
 }
