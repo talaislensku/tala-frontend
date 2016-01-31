319c3814c57ec9a86ecc5d59c2ee716672dad26c
David Blurton <david.blurton@gmail.com>
2016-01-31 17:16:37 +0000

    Style language picker

--- app/language-picker.js
+++ app/language-picker.js
@@ -4,10 +4,13 @@ import styles from './language-picker.css'
 export default ({lang, onChange}) => {
   return (
     <div className={styles.root}>
-      <select type="select" value={lang} onChange={onChange}>
-        <option value="en">English</option>
-        <option value="is">Íslenska</option>
-      </select>
+      <span className={styles.arrow}>▾</span>
+      <div className={styles.select}>
+        <select type="select" value={lang} onChange={onChange}>
+          <option value="en">English</option>
+          <option value="is">Íslenska</option>
+        </select>
+      </div>
     </div>
   )
 }
