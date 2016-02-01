ac362d015e65780c46754e6d6ee3e831e392d339
David Blurton <david.blurton@gmail.com>
2016-02-01 00:18:05 +0000

    Remove extra comma

--- app/footer.js
+++ app/footer.js
@@ -4,7 +4,7 @@ import styles from './footer.css'
 export default () => (
   <footer className={styles.root}>
     <p>
-      Data from <a href="http://bin.arnastofnun.is/" target="_blank">Beygingarlýsing íslensks nútímamáls</a>, and <a href="https://github.com/vthorsteinsson/Reynir" target="_blank">Reynir</a>.
+      Data from <a href="http://bin.arnastofnun.is/" target="_blank">Beygingarlýsing íslensks nútímamáls</a> and <a href="https://github.com/vthorsteinsson/Reynir" target="_blank">Reynir</a>.
     </p>
     <p>
       tala.islensku is an experimental project by <a href="http://davidblurton.com" target="_blank">David Blurton</a>.
