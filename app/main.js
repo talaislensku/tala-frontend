d33f9e21460ff77c95b9b12c5e924812de83c2ed
David Blurton <david.blurton@gmail.com>
2016-02-22 23:05:22 +0000

    Refactor word component

--- app/main.js
+++ app/main.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import axios from 'axios'
 import classNames from 'classnames'
 import { createHistory, useQueries } from 'history'
-import debounce from 'lodash.debounce'
 import { TranslatorProvider } from "react-translate"
 
 import styles from './main.css'
@@ -12,55 +10,11 @@ import SeeAlso from './see-also'
 import Suggestions from './suggestions'
 import Logo from './logo'
 import Footer from './footer'
+import Word from './word'
 import translations from '../translations.yaml'
 
 const isMobile = 'ontouchstart' in window
 const api = window.location.hostname === 'localhost' ? 'http://localhost:8000' : 'https://api.tala.is'
-
-function getBestMatch(data, query) {
-  return data.filter(word => word.headWord === query)[0] ||
-    data.filter(word => word.forms.some(form => form.form === query))[0] ||
-    data[0]
-}
-
-function getBestFormMatch(match, query) {
-  if (!(match && match.forms)) {
-    return {}
-  }
-
-  return match && match.forms.filter(x => x.form.toLowerCase() === query.toLowerCase())[0]
-}
-
-function getMatchingForm(match, tag) {
-  if (!(match && match.forms)) {
-    return {}
-  }
-
-  return match && match.forms.filter(x => x.grammarTag === tag)[0]
-}
-
-const lookupWord = (function() {
-  let current;
-
-  function getWord(word, lang) {
-    current = word
-
-    let promise = new Promise((resolve, reject) => {
-      axios.get(`${api}/find/${word}?lang=${lang}`)
-        .then(result => {
-          if (promise.word === current) {
-            resolve(result)
-          }
-        })
-    })
-
-    promise.word = word
-
-    return promise
-  }
-
-  return getWord
-})()
 
 export class Main extends React.Component {
   constructor(props) {
@@ -72,7 +26,6 @@ export class Main extends React.Component {
     }
 
     this.history = useQueries(createHistory)()
-    this.getSuggestionsDebounced = debounce(this.getSuggestions, 500)
   }
 
   queryChanged = (event) => {
@@ -85,102 +38,23 @@ export class Main extends React.Component {
     })
   };
 
-  navigate = ({q: query, id, tag}) => {
-    this.setState({
-      query,
-    })
-
-    if (!query) {
-      this.clearResults()
-      this.getSuggestionsDebounced.cancel()
-      return
-    }
-
-    lookupWord(query, this.state.lang)
-      .then(res => this.handleResponse(res, {query, id, tag}))
-  };
-
-  getSuggestions = (query) => {
-    return axios.get(`${api}/suggestions/${query}`)
-      .then(({data}) => {
-        this.setState({
-          suggestions: data
-        })
-      })
-  };
-
-  clearResults = () => {
-    this.setState({
-      result: null,
-      current: null,
-      otherMatches: null,
-      suggestions: null,
-    })
-  };
-
-  handleResponse = ({data}, {query, id, tag}) => {
-    if (!data) {
-      return
-    }
-
-    if (data.length === 0) {
-      // check if query matches start of a result to reduce flicker
-
-      this.getSuggestionsDebounced(query)
-      this.clearResults()
-      return
-    }
-
-    let bestMatch = data.filter(d => d.binId == id)[0] || getBestMatch(data, query)
-    let otherMatches = data.filter(x => x !== bestMatch)
-    let current = getMatchingForm(bestMatch, tag) || getBestFormMatch(bestMatch, query)
-
-    if (bestMatch) {
-      this.getSuggestionsDebounced.cancel()
-      this.setState({
-        result: bestMatch,
-        current,
-        otherMatches,
-        data,
-        suggestions: []
-      })
-    }
-  };
-
   onLanguageChange = (event) => {
     let lang = event.target.value
     localStorage.setItem('lang', lang)
     this.setState({ lang }, () => this.navigate({q: this.state.query}))
   };
 
-  setCurrentForm = (current) => {
-    this.history.replace({
-      query: {
-        q: current.form,
-        id: this.state.result.binId,
-        tag: current.grammarTag,
-      }
-    })
+  onWordChange = (word) => {
+    this.history.replace(word)
 
     if (!isMobile) {
       this.refs.search.focus()
     }
   };
 
-  setCurrent = (result) => {
-    this.history.replace({
-      query: {
-        q: this.state.query,
-        id: result.binId,
-      }
-    })
-  };
-
-  setSuggestion = (suggestion) => {
-    this.history.replace({
-      query: {
-        q: suggestion,
-      }
+  navigate = ({q: query}) => {
+    this.setState({
+      query,
     })
   };
 
@@ -197,7 +71,7 @@ export class Main extends React.Component {
   }
 
   render() {
-    let {query, result, current, otherMatches, suggestions} = this.state
+    let {query, lang} = this.state
     const t = translations[this.state.lang]
 
     return (
@@ -206,18 +80,8 @@ export class Main extends React.Component {
           <div className={styles.content}>
             <Logo />
             <input ref="search" type="text" className={styles.search} value={query} onChange={this.queryChanged} placeholder={t.ui['search-for-an-icelandic-word']} autoCapitalize="none" />
-            { result && <div>
-              <span className={styles.headWord}>{result.headWord}</span>
-              <span className={styles.wordClass}>{result.wordClass}</span>
-            </div> }
-
-            { query && <div>
-              <Results {...this.state} setCurrentForm={this.setCurrentForm} />
-              <SeeAlso result={result} otherMatches={otherMatches} setCurrent={this.setCurrent} />
-              <Suggestions suggestions={suggestions} navigate={this.setSuggestion} />
-            </div> }
-
-            <LanguagePicker lang={this.state.lang} onChange={this.onLanguageChange} />
+            <Word query={query} lang={lang} onChange={this.onWordChange} />
+            <LanguagePicker lang={lang} onChange={this.onLanguageChange} />
           </div>
           <Footer />
         </div>
---
+++ app/word.js
@@ -0,0 +1,186 @@
+import React from 'react'
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+
+import styles from './main.css'
+import Results from './results'
+import SeeAlso from './see-also'
+import Suggestions from './suggestions'
+
+const api = window.location.hostname === 'localhost' ? 'http://localhost:8000' : 'https://api.tala.is'
+
+function getBestMatch(data, query) {
+  return data.filter(word => word.headWord === query)[0] ||
+    data.filter(word => word.forms.some(form => form.form === query))[0] ||
+    data[0]
+}
+
+function getBestFormMatch(match, query) {
+  if (!(match && match.forms)) {
+    return {}
+  }
+
+  return match && match.forms.filter(x => x.form.toLowerCase() === query.toLowerCase())[0]
+}
+
+function getMatchingForm(match, tag) {
+  if (!(match && match.forms)) {
+    return {}
+  }
+
+  return match && match.forms.filter(x => x.grammarTag === tag)[0]
+}
+
+const lookupWord = (function() {
+  let current;
+
+  function getWord(word, lang) {
+    current = word
+
+    let promise = new Promise((resolve, reject) => {
+      axios.get(`${api}/find/${word}?lang=${lang}`)
+        .then(result => {
+          if (promise.word === current) {
+            resolve(result)
+          }
+        })
+    })
+
+    promise.word = word
+
+    return promise
+  }
+
+  return getWord
+})()
+
+export default class Word extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+
+    this.getSuggestionsDebounced = debounce(this.getSuggestions, 500)
+  }
+
+  static propTypes = {
+    query: React.PropTypes.string,
+    lang: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+  };
+
+  navigate = ({q: query, id, tag}) => {
+    if (!query) {
+      this.clearResults()
+      this.getSuggestionsDebounced.cancel()
+      return
+    }
+
+    lookupWord(query, this.props.lang)
+      .then(res => this.handleResponse(res, {query, id, tag}))
+  };
+
+  getSuggestions = (query) => {
+    return axios.get(`${api}/suggestions/${query}`)
+      .then(({data}) => {
+        this.setState({
+          suggestions: data
+        })
+      })
+  };
+
+  clearResults = () => {
+    this.setState({
+      result: null,
+      current: null,
+      otherMatches: null,
+      suggestions: null,
+    })
+  };
+
+  handleResponse = ({data}, {query, id, tag}) => {
+    if (!data) {
+      return
+    }
+
+    if (data.length === 0) {
+      // check if query matches start of a result to reduce flicker
+
+      this.getSuggestionsDebounced(query)
+      this.clearResults()
+      return
+    }
+
+    let bestMatch = data.filter(d => d.binId == id)[0] || getBestMatch(data, query)
+    let otherMatches = data.filter(x => x !== bestMatch)
+    let current = getMatchingForm(bestMatch, tag) || getBestFormMatch(bestMatch, query)
+
+    if (bestMatch) {
+      this.getSuggestionsDebounced.cancel()
+      this.setState({
+        result: bestMatch,
+        current,
+        otherMatches,
+        data,
+        suggestions: []
+      })
+    }
+  };
+
+  setCurrentForm = (current) => {
+    this.props.onChange({
+      query: {
+        q: current.form,
+        id: this.state.result.binId,
+        tag: current.grammarTag,
+      }
+    })
+  };
+
+  setCurrent = (result) => {
+    this.props.onChange({
+      query: {
+        q: this.props.query,
+        id: result.binId,
+      }
+    })
+  };
+
+  setSuggestion = (suggestion) => {
+    this.props.onChange({
+      query: {
+        q: suggestion,
+      }
+    })
+  };
+
+  componentDidMount() {
+    this.navigate({q: this.props.query})
+  }
+
+  componentWillReceiveProps(props) {
+    this.navigate({q: props.query})
+  }
+
+  render() {
+    let {query} = this.props
+    let {result, current, otherMatches, suggestions} = this.state
+
+    return (
+      <div>
+        { result && <div>
+          <span className={styles.headWord}>{result.headWord}</span>
+          <span className={styles.wordClass}>{result.wordClass}</span>
+        </div> }
+
+        { query && <div>
+          <Results {...this.state} setCurrentForm={this.setCurrentForm} />
+          <SeeAlso result={result} otherMatches={otherMatches} setCurrent={this.setCurrent} />
+          <Suggestions suggestions={suggestions} navigate={this.setSuggestion} />
+        </div> }
+      </div>
+    )
+  }
+}
