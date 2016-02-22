c4a59632a2be68cbf9bd3344d0ddd20bb23ec0e0
David Blurton <david.blurton@gmail.com>
2016-02-22 00:23:07 +0000

    Fix flattening

--- app/see-also.js
+++ app/see-also.js
@@ -2,11 +2,11 @@ import React from 'react'
 import styles from './main.css'
 import { translate } from 'react-translate'
 
-const SeeAlso = ({t, otherMatches, result, setCurrent}) => (
+const SeeAlso = ({t, otherMatches, setCurrent}) => (
   otherMatches && otherMatches.length ?
     <div className={styles.seeAlso}>{t('see-also')}:
     { otherMatches.map(result => {
-      return <div key={result.wordClass}>
+      return <div key={result.headWord + result.wordClass}>
         <div className={styles.link} onClick={setCurrent.bind(null, result)}>{result.headWord} {result.wordClass}</div>
       </div>
     }) }
